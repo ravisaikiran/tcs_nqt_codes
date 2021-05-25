2867b76bc9a52305b047fd83264da38784c02257
ravisaikiran <83855569+ravisaikiran@users.noreply.github.com>
2021-05-25 22:03:51 +0530

    Create removing_subStrings.py

---
+++ removing_subStrings.py
@@ -0,0 +1,17 @@
+#input string1=xxyzyzyv
+#      string2=xyz
+#output   2(len of string after removing str2 from str1 recursively)
+def fun(s1,s2):
+    if s2 not in s1:
+        return len(s1)
+    else:
+        i=s1.index(s2)
+        s1=s1[:i]+s1[i+len(s2):]
+        return fun(s1,s2)
+       
+        
+    
+string1=input()
+string2=input()
+x=fun(string1,string2)
+print(x)
