8d9e9f5386304705e182266471cd38b6ca524eb7
ravisaikiran <83855569+ravisaikiran@users.noreply.github.com>
2021-05-25 22:04:41 +0530

    Update removing_subStrings.py

--- removing_subStrings.py
+++ removing_subStrings.py
@@ -8,9 +8,6 @@ def fun(s1,s2):
         i=s1.index(s2)
         s1=s1[:i]+s1[i+len(s2):]
         return fun(s1,s2)
-       
-        
-    
 string1=input()
 string2=input()
 x=fun(string1,string2)
